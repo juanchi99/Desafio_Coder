8608f7bfc6bcf6b34bbe65ed3e9aca13818062c3
Juan Martinez <88692518+juanchi99@users.noreply.github.com>
2023-09-02 17:03:39 -0300

    Add files via upload

--- script.py
+++ script.py
@@ -1,27 +1,27 @@
 #Importamos las librerias necesarias para que corra el codigo
 
-import yfinance as yf
-import requests
-import psycopg2
-import sqlalchemy
-from sqlalchemy import create_engine
 import pandas as pd
-from getpass import getpass
+import yfinance as yf
+from sqlalchemy import create_engine
+import os
+import dotenv
 
-# CREANDO LA CONEXION
+# cargamos las variables de entorno
+dotenv.load_dotenv()  
 
-password = getpass("Introduce tu contraseña: ")
-user = "juanchi_martinezc_coderhouse"
-host = "data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com"
-port = "5439"
-database_name = "data-engineer-database"
+# leemos las varaibles de entorno
+user = os.getenv('user')
+password = os.getenv('password')
+host = os.getenv('host')
+port = os.getenv('port')
+database_name = os.getenv('database_name')
 
 connection_string = f"postgresql://{user}:{password}@{host}:{port}/{database_name}"
 conn = create_engine(connection_string)
 
 # Insertamos la informacion de los Tickers a Redshift
 
-tickers = ['AAPL','MSFT','AMZN','NVDA','GOOGL','META','TSLA','UNH','JPM']
+tickers = ['AAPL','MSFT','AMZN','NVDA','GOOGL','META','TSLA','UNH','JPM','CSCO']
 
 # Diccionario para almacenar los datos
 ticker_data = {}
@@ -31,14 +31,16 @@ for ticker in tickers:
     data = yf.download(ticker, period="1y")
     data.reset_index(inplace=True)
     data.drop(columns=['Close'], inplace=True)
+    data['Ticker'] = ticker
 
     # Almacena los datos en el diccionario usando el ticker como clave
     ticker_data[ticker] = data
     
-# Ahora puedes acceder a los datos individualmente usando los tickers como claves
-ticker_data['AAPL'].head()  # Para acceder a los datos de AAPL
+# Concatenar los datos en una sola tabla
+all_data = pd.concat(list(ticker_data.values()), ignore_index=True)
+
+# Imprimir los primeros registros para verificar
+print(all_data)
 
 # Iterar a través de los datos de los tickers y cargarlos en Redshift
-for ticker, data in ticker_data.items():
-    table_name = f'hist_{ticker}'
-    data.to_sql(table_name, conn, index=False, if_exists='append')
+all_data.to_sql('TICKERS', conn, index=False, if_exists='append')
